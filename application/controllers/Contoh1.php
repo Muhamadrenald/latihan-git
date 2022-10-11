ac0eeb0c26486302f88a040337a87d339c03e713
Muhamadrenald <avryso@gmail.com>
2022-10-11 17:29:17 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhamad Renald
+        saya tinggal di Jogja
+        olahraga yang saya sukai adalah lari";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Muhamad</li>
+        <li>Nama Belakang : Renald</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="disc">
+        <li> Bantul Yogyakarta</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="square">
+        <li>Surabaya</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutabgkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
