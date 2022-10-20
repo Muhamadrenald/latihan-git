4be1ca6edf02910692e4f7c228ccd13dcd95df96
Muhamadrenald <avryso@gmail.com>
2022-10-20 16:06:10 +0700

    commit ke 2

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -26,14 +26,14 @@
                     <th>Kode MTK</th>
                     <th>:</th>
                     <td>
-                        <input type="text" name="kode" id="kode">
+                        <input type="text" name="kode" id="kode" placeholder="Required">
                     </td>
                 </tr>
                 <tr>
                     <th>Nama MTK</th>
                     <td>:</td>
                     <td>
-                        <input type="text" name="nama" id="nama">
+                        <input type="text" name="nama" id="nama" placeholder="Required">
                     </td>
                 </tr>
                 <tr>
